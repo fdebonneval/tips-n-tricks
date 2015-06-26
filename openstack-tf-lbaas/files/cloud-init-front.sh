79ee8bed243a84eba3e90778bd718c8817761f19
Foucault de Bonneval <foucault@commit.ninja>
2015-06-26 17:57:45 +0200

    LBaaS terraform stack

---
+++ openstack-tf-lbaas/files/cloud-init-front.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y nginx openssl
+
+mkdir /etc/nginx/ssl
+
+cat > /etc/nginx/ssl/server.key <<EOF
+-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDMLgOO3nVciTMB
+//53TONoheAaRIQ70NmbHrwzdycf/yTLv2D5nrYHP40HPJz1deR8tkTMX0omjVkT
+SZFk1I/fAraz3XwtAGGsSUfZknL9xjtCBLMbqd721e9o8DGhVy+J1K+EtoFNcHih
+1CaJbhg3yjHHQo+YEz2cD9MsEqIudPnd2l8E4upsYQ7i39bYAUtbXwK01xz3OVdO
+xcUaMgVcsnbMJbij6qxFp8JlwxolpoOCpraa2U8UzoSWUdUOWD/8WDrO09Rat4kB
+M+AKtil17iRJg9+Jnn8V1o0OOCaJpA998aFnuPR/E85pi42S2dgzrsarelQ0giYX
+Okr9RRphAgMBAAECggEAeXBIt3nCbeZAS0k5zTUS8IDnmFf2GimOs38lSqrsv1Ae
+T3ylLfJiq471zz8Sz79txWsXIkLutF1PhHQ2ILV4WJihl/r8tztJ3JG9RT+gWyXC
+6vImoSZ4sXDuswhhTGexo5W3SwTNhQSHCyFGRmkPyGbfEneZNkcDVsXmwIjYoy3p
+Fb9iyqXC3Lpso1XobeCN0K+nfUPf1Kb6eIdamdAbCiEkndZ/7MVQdCu/C+QnJ0aF
++/N1FSAMfYhdikKFp02h3d9qUk03/HFMJzXDOensD1hk6un0hpDumK6DamuyfCpY
+lnfYXW0HW773U9t+1L1KqlRwoaxg7lzPbPA31hBETQKBgQD2N9QrhHCDerkIaYY0
+8gpnTf86+pbO7bREUGuIu31Ls98UVeKkmW+5xEfckKp1zaqHXCmVB5512EID+5mC
+saYWLvd25gxmEvfDt74BbcG2p+EONZQdDGVksTdB6Yy0Cw9cv5jKrxO5ut6NLHcS
+mYsWLbPe1ngvlD6SyQ9rJugRzwKBgQDUSqHnOZdwtPMkvu/vM2j+EKlUfFec3Xch
+Wol52ubleisDAF9EpXEvsWBMBvnmB8hNcQP1Pw00vVdX6eszqNbbBtRwmBXhHHCy
+7VSEKL1R4tkMko4Of+ShCewkDZcdTMk/XU6sU0wv2j2F5CsgrIE/TAej98ixJHIq
+6hQ5W+cMzwKBgQCGR8Bg02QBcMbE/bgB3Bcsa+9MnSnuRNlRgIKFGaulw71f+88V
+cdDrAU8nzYYJpVbhZ7QN4Q/cuUqXnXoFOxmXc5nmsGQGr0WM6gKoCNHi6f/lnfbf
+OMl7duLqAZOJBZclQCD2OTGK041YdO2jqTWfrOEyIMl/OVw+9YSBLcDXJQKBgCHD
+SOKUZ0B2luD2OQSrAXy+u5+DMw/wrPyyAIFPzj8a4fJVQdGSGmFCbZVJ2r656CJg
+4gdEIt6LanPB8TVDGgC1ol3R4lDuDAJ4+mMWc52tXWXBfTRTJNJz2ImXW7w+NReN
+yHBhwxEtPXGo6y8EIH4nomNyigmaSUoH9nV051dDAoGAaQXZ73ZAlcDmaIz4BngW
+Rr8HwBEcthSl2KGutXpu+M9FsnCgf31Gt1j/TWAAvxdGA8NDeaoVzEz8Z2VL12ej
+eI/1GfqvIKQ23t7wCzibGiKgG+HT8Ij6zDgL9TU91+n8AOxDf3+hWY4nse5jzq1F
+WkTmxMAdb3+COkzYPVh+u/s=
+-----END PRIVATE KEY-----
+EOF
+
+cat > /etc/nginx/ssl/server.crt <<EOF
+-----BEGIN CERTIFICATE-----
+MIIDezCCAmOgAwIBAgIJAKyn7JJ79Dn3MA0GCSqGSIb3DQEBCwUAMFQxCzAJBgNV
+BAYTAkZSMQwwCgYDVQQIDANJREYxDjAMBgNVBAcMBVBBUklTMRIwEAYDVQQKDAlD
+bG91ZHdhdHQxEzARBgNVBAMMCm15LWxiLXRlc3QwHhcNMTUwNjI1MTEwMDMxWhcN
+MTYwNjI0MTEwMDMxWjBUMQswCQYDVQQGEwJGUjEMMAoGA1UECAwDSURGMQ4wDAYD
+VQQHDAVQQVJJUzESMBAGA1UECgwJQ2xvdWR3YXR0MRMwEQYDVQQDDApteS1sYi10
+ZXN0MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzC4Djt51XIkzAf/+
+d0zjaIXgGkSEO9DZmx68M3cnH/8ky79g+Z62Bz+NBzyc9XXkfLZEzF9KJo1ZE0mR
+ZNSP3wK2s918LQBhrElH2ZJy/cY7QgSzG6ne9tXvaPAxoVcvidSvhLaBTXB4odQm
+iW4YN8oxx0KPmBM9nA/TLBKiLnT53dpfBOLqbGEO4t/W2AFLW18CtNcc9zlXTsXF
+GjIFXLJ2zCW4o+qsRafCZcMaJaaDgqa2mtlPFM6EllHVDlg//Fg6ztPUWreJATPg
+CrYpde4kSYPfiZ5/FdaNDjgmiaQPffGhZ7j0fxPOaYuNktnYM67Gq3pUNIImFzpK
+/UUaYQIDAQABo1AwTjAdBgNVHQ4EFgQUv8qwQnlMc6hqYlZhHgFZk2eFyRUwHwYD
+VR0jBBgwFoAUv8qwQnlMc6hqYlZhHgFZk2eFyRUwDAYDVR0TBAUwAwEB/zANBgkq
+hkiG9w0BAQsFAAOCAQEARGS59iGxYQTN3HLnQPpdui4dZ62jbcArjuZUht1Mnqoa
+ODq3NlFP63MtPLZzJzTdW48TVbomyKHYudq114GLEbPULhPXiEpjwZoL8qYdSGCk
+iJMgp03HgxDtrVruH/ffHUidPLtk11RC5pxXbVOEEq0clJRd2KFozCycrirVvnHq
+QNrg8yMty+8zudvEaiG6mo48yb553ajk3E8RevuJQvQpBOPwVhslLghzyC9AXyTF
+XszoYWXUIn5Xe2vfmB83GqHKJqPGw7ro5YtpmNLQDiuOuzbY0Jipj8skX32emS2K
+NjqcbqS5uya2kXeGkUCZ8092FfNvizKUXiPmoNzTtA==
+-----END CERTIFICATE-----
+EOF
+cat > /usr/share/nginx/html/index.html <<EOF
+$(hostname)
+EOF
+
+cat > /etc/nginx/sites-available/default <<EOF
+server {
+        listen 80;
+
+        listen 443 ssl;
+
+        root /usr/share/nginx/html;
+        index index.html index.htm;
+
+        ssl_certificate /etc/nginx/ssl/server.crt;
+        ssl_certificate_key /etc/nginx/ssl/server.key;
+}
+EOF
+
+service nginx restart
---
+++ openstack-tf-lbaas/openstack-lbaas.tf
@@ -0,0 +1,184 @@
+provider "openstack" {
+# endpoints, credentials are taken from environment variables
+  insecure = "true"
+}
+
+# Networks creation
+resource "openstack_networking_network_v2" "tf-net-north" {
+  name = "tf-net-north"
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_network_v2" "tf-net-lb-pool" {
+  name = "tf-net-lb-pool"
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_subnet_v2" "tf-subnet-north" {
+  network_id = "${openstack_networking_network_v2.tf-net-north.id}"
+  cidr = "10.128.0.0/24"
+  ip_version = 4
+}
+
+resource "openstack_networking_subnet_v2" "tf-subnet-lb-pool" {
+  network_id = "${openstack_networking_network_v2.tf-net-lb-pool.id}"
+  cidr = "10.254.0.0/24"
+  ip_version = 4
+}
+
+# Floatingips creation
+resource "openstack_networking_floatingip_v2" "tf-floating-bst" {
+  pool = "public"
+}
+
+resource "openstack_networking_floatingip_v2" "tf-floating-00" {
+  pool = "public"
+}
+
+# Router creation
+resource "openstack_networking_router_v2" "tf-router-00" {
+  name = "tf-router-00"
+  admin_state_up = "true"
+  external_gateway = "4a0d5db1-f30f-491c-b342-af80143a31ea"
+}
+
+resource "openstack_networking_router_interface_v2" "tf-router-north" {
+    router_id = "${openstack_networking_router_v2.tf-router-00.id}"
+    subnet_id = "${openstack_networking_subnet_v2.tf-subnet-north.id}"
+}
+
+# Security groups
+resource "openstack_compute_secgroup_v2" "tf-sg-icmp-ssh" {
+  name = "tf-sg-icmp-ssh"
+  description = "ICMP and SSH Security groups"
+  rule {
+    ip_protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+    cidr = "0.0.0.0/0"
+  }
+  rule {
+    ip_protocol = "icmp"
+    from_port = "-1"
+    to_port = "-1"
+    cidr = "0.0.0.0/0"
+  }
+}
+
+resource "openstack_compute_secgroup_v2" "tf-sg-80-443" {
+  name = "tf-sg-80-443"
+  description = "80 and 443 Security groups"
+  rule {
+    ip_protocol = "tcp"
+    from_port = "80"
+    to_port = "80"
+    cidr = "0.0.0.0/0"
+  }
+  rule {
+    ip_protocol = "tcp"
+    from_port = "443"
+    to_port = "443"
+    cidr = "0.0.0.0/0"
+  }
+}
+
+# Create bastion server
+resource "openstack_compute_instance_v2" "tf-bst-00" {
+  name = "tf-bst-00"
+  network {
+    uuid = "${openstack_networking_network_v2.tf-net-north.id}"
+  }
+  image_id = "5db66a8a-3165-4606-982d-43e89846c16f"
+  flavor_id = "3"
+  key_pair = "foucault"
+  security_groups = ["tf-sg-icmp-ssh"]
+}
+
+# Create front server
+resource "openstack_compute_instance_v2" "tf-front-00" {
+  name = "tf-front-00"
+  network {
+    uuid = "${openstack_networking_network_v2.tf-net-north.id}"
+  }
+  image_id = "5db66a8a-3165-4606-982d-43e89846c16f"
+  flavor_id = "3"
+  key_pair = "foucault"
+  security_groups = ["tf-sg-icmp-ssh","tf-sg-80-443"]
+  user_data = "${file(\"files/cloud-init-front.sh\")}"
+}
+
+resource "openstack_compute_instance_v2" "tf-front-01" {
+  name = "tf-front-01"
+  network {
+    uuid = "${openstack_networking_network_v2.tf-net-north.id}"
+  }
+  image_id = "5db66a8a-3165-4606-982d-43e89846c16f"
+  flavor_id = "3"
+  key_pair = "foucault"
+  security_groups = ["tf-sg-icmp-ssh","tf-sg-80-443"]
+  user_data = "${file(\"files/cloud-init-front.sh\")}"
+}
+
+# LBaaS HTTP pool creation
+resource "openstack_lb_pool_v1" "tf-lbpool-00" {
+  name = "tf-lbpool-00"
+  protocol = "HTTP"
+  subnet_id = "${openstack_networking_subnet_v2.tf-subnet-north.id}"
+  lb_method = "ROUND_ROBIN"
+  member {
+    address = "${openstack_compute_instance_v2.tf-front-00.access_ip_v4}"
+    port = 80
+    admin_state_up = "true"
+  }
+  member {
+    address = "${openstack_compute_instance_v2.tf-front-01.access_ip_v4}"
+    port = 80
+    admin_state_up = "true"
+  }
+}
+
+# LBaaS HTTP VIP creation
+resource "openstack_lb_vip_v1" "tf-lbvip-00" {
+  name = "tf-lbvip-00"
+  subnet_id = "${openstack_networking_subnet_v2.tf-subnet-lb-pool.id}"
+  protocol = "HTTP"
+  port = 80
+  pool_id = "${openstack_lb_pool_v1.tf-lbpool-00.id}"
+#  floating_ip = "${openstack_networking_floatingip_v2.tf-floating-00.id}"
+}
+
+# LBaaS tcp/443 pool creation
+resource "openstack_lb_pool_v1" "tf-lbpool-01" {
+  name = "tf-lbpool-01"
+  protocol = "TCP"
+  subnet_id = "${openstack_networking_subnet_v2.tf-subnet-north.id}"
+  lb_method = "ROUND_ROBIN"
+  member {
+    address = "${openstack_compute_instance_v2.tf-front-00.access_ip_v4}"
+    port = 443
+    admin_state_up = "true"
+  }
+  member {
+    address = "${openstack_compute_instance_v2.tf-front-01.access_ip_v4}"
+    port = 443
+    admin_state_up = "true"
+  }
+}
+
+# LBaaS tcp/443 VIP creation
+resource "openstack_lb_vip_v1" "tf-lbvip-01" {
+  name = "tf-lbvip-01"
+  subnet_id = "${openstack_networking_subnet_v2.tf-subnet-lb-pool.id}"
+  protocol = "TCP"
+  port = 443
+  pool_id = "${openstack_lb_pool_v1.tf-lbpool-01.id}"
+#  floating_ip = "${openstack_networking_floatingip_v2.tf-floating-00.id}"
+}
+
+# Outputs
+output "bst-floatingip" {
+  value = "neutron floatingip-associate ${openstack_networking_floatingip_v2.tf-floating-bst.id} ${openstack_compute_instance_v2.tf-bst-00.access_ip_v4}"
+}
+output "lb-floatingip" {
+  value = "neutron floatingip-associate ${openstack_networking_floatingip_v2.tf-floating-00.id} [LB_VIP_PORT_ID]"
+}
