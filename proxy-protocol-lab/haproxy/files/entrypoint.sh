388187ba7e7b9848f243ecf06e2116c213743400
Foucault de Bonneval <foucault@commit.ninja>
2015-09-03 19:17:42 +0200

    relou

--- proxy-protocol-lab/haproxy/files/entrypoint.sh
+++ proxy-protocol-lab/haproxy/files/entrypoint.sh
@@ -5,8 +5,9 @@ CONSUL_TEMPLATE_PATH=$1
 ${CONSUL_TEMPLATE_PATH}/consul-template -consul=${CONSUL_PORT_8500_TCP_ADDR}:8500 -template="${CONSUL_TEMPLATE_PATH}/haproxy.ctmpl:/etc/haproxy/haproxy.cfg:"&
 
 /usr/sbin/haproxy -D -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid
+sleep 1
 
-while true
+while test -f /var/run/haproxy.pid
 do SUM=$(md5sum /etc/haproxy/haproxy.cfg | awk '{print $1}')
   if [ "${SUM}" != "${SUM0}" ]
   then
@@ -16,3 +17,5 @@ do SUM=$(md5sum /etc/haproxy/haproxy.cfg | awk '{print $1}')
   fi
   sleep 5
 done
+echo "terminated"
+kill %1
