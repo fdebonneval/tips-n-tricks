38cb2e10bfcdeced34da116039628a8de90085a8
Foucault de Bonneval <foucault@commit.ninja>
2015-09-03 19:05:27 +0200

    typo in haproxy path

--- proxy-protocol-lab/haproxy/files/entrypoint.sh
+++ proxy-protocol-lab/haproxy/files/entrypoint.sh
@@ -4,7 +4,7 @@ CONSUL_TEMPLATE_PATH=$1
 
 ${CONSUL_TEMPLATE_PATH}/consul-template -consul=${CONSUL_PORT_8500_TCP_ADDR}:8500 -template="${CONSUL_TEMPLATE_PATH}/haproxy.ctmpl:/etc/haproxy/haproxy.cfg:"&
 
-/sur/sbin/haproxy -D -f /etc/haproxy/haproxy.cfg
+/usr/sbin/haproxy -D -f /etc/haproxy/haproxy.cfg
 
 while true
 do SUM=$(md5sum /etc/haproxy/haproxy.cfg | awk '{print $1}')
