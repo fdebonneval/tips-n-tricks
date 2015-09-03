33aa9c1a739d0ba2f41a158ddc7ca6898d15b542
Foucault de Bonneval <foucault@commit.ninja>
2015-09-03 19:07:07 +0200

    relou

--- proxy-protocol-lab/haproxy/files/entrypoint.sh
+++ proxy-protocol-lab/haproxy/files/entrypoint.sh
@@ -4,14 +4,14 @@ CONSUL_TEMPLATE_PATH=$1
 
 ${CONSUL_TEMPLATE_PATH}/consul-template -consul=${CONSUL_PORT_8500_TCP_ADDR}:8500 -template="${CONSUL_TEMPLATE_PATH}/haproxy.ctmpl:/etc/haproxy/haproxy.cfg:"&
 
-/usr/sbin/haproxy -D -f /etc/haproxy/haproxy.cfg
+/usr/sbin/haproxy -D -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid
 
 while true
 do SUM=$(md5sum /etc/haproxy/haproxy.cfg | awk '{print $1}')
   if [ "${SUM}" != "${SUM0}" ]
   then
     echo "config file changed, reloading"
-    kill -HUP $(cat /var/run/haproxy)
+    kill -HUP $(cat /var/run/haproxy.pid)
     SUM0=${SUM}
   fi
   sleep 5
